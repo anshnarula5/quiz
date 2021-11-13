e8bf8a25a9f641192faa4bd1a02547b26c620c22
Ansh Narula <2019210@iiitdmj.ac.in>
2021-11-13 23:17:37 +0530

    styling and scores

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,39 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Quiz from "./Quiz";
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Grid} from "@mui/material";
+import { Container } from "@mui/material";
+
 const Main = () => {
-  const [quizData, setQuizData] = useState([])
+  const [quizData, setQuizData] = useState([]);
   const fetchData = () => {
     fetch("https://opentdb.com/api.php?amount=10&type=multiple")
-      .then(res => res.json())
-    .then(data => setQuizData(data))
-  }
+      .then((res) => res.json())
+      .then((data) => setQuizData(data));
+  };
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
   return (
     <>
-      {quizData && quizData.response_code === 0 && <Quiz data={quizData.results}/>}
+      <Container maxWidth="lg">
+      <Grid container sx = {{my : 3}}  spacing={3}>
+        <Grid item xs={8}>
+          {quizData && quizData.response_code === 0 && (
+            <Card sx={{ minWidth: 275 }}>
+              <CardContent>
+                <Quiz data={quizData.results} />
+              </CardContent>
+            </Card>
+          )}
+        </Grid>
+        <Grid item xs={4}>
+          sadadad
+        </Grid>
+      </Grid>
+      </Container>
     </>
   );
 };
--- src/components/Main/Quiz.js
+++ src/components/Main/Quiz.js
@@ -1,54 +1,71 @@
 import React, { useState } from "react";
 import { decode } from "html-entities";
 
-import { Button } from "@mui/material";
+import { Button, Container, Grid } from "@mui/material";
 
 const Quiz = ({ data }) => {
   const [question, setQuestion] = useState("");
   const [qNo, setQNo] = useState(0);
   const [answer, setAnswer] = useState("")
-
-  const nextQ = () => {setQNo((initial) => initial + 1);};
+  const [submit, setSubmit] = useState(false)
+  const [score, setScore] = useState(0)
+  const [correct, setCorrect] = useState(false)
   
+    const handleSubmit = () => {
+        setSubmit(true)
+        if (answer === data[qNo].correct_answer) {
+            setCorrect(true)
+            setScore(initial => initial + 1)
+        }
+    }
+
+    const nextQ = () => {setQNo((initial) => initial + 1);};
+    
   const handleSelect = (option) => {
         setAnswer(option)
+    }
+    const handleNext = () => {
+        nextQ()
+        setAnswer("")
+       setSubmit(false)
     }
     const options = [...data[qNo].incorrect_answers, data[qNo].correct_answer];
   return (
     <>
       <h1>Question {qNo + 1}</h1>
+      <h1>Score : {score}</h1>
       <h1>{decode(data[qNo].question)}</h1>
-      <div>
-          <Button variant = {answer !== options[0] ? "outlined" : "contained"}
+      <div >
+          <Button variant = {answer !== options[0] ? "outlined" : "contained"} disabled = {submit ? true : false}  
                 sx={{
                     display: "block", width: "20rem",p: 2, my: 2}}
-                    onClick = {() => handleSelect(options[0])}>
+                    onClick = {() => handleSelect(options[0])}  item xs={8}>
             {options[0]}
           </Button>
-          <Button variant = {answer !== options[1] ? "outlined" : "contained"}
+          <Button variant = {answer !== options[1] ? "outlined" : "contained"} disabled = {submit ? true : false} 
                 sx={{
                     display: "block", width: "20rem",p: 2, my: 2}}
                     onClick = {() => handleSelect(options[1])}>
             {options[1]}
           </Button>
-          <Button variant = {answer !== options[2] ? "outlined" : "contained"}
+          <Button variant = {answer !== options[2] ? "outlined" : "contained"} disabled = {submit ? true : false} 
                 sx={{
                     display: "block", width: "20rem",p: 2, my: 2}}
                     onClick = {() => handleSelect(options[2])}>
             {options[2]}
           </Button>
-          <Button variant = {answer !== options[3] ? "outlined" : "contained"}
+          <Button variant = {answer !== options[3] ? "outlined" : "contained"} disabled = {submit ? true : false} 
                 sx={{
                     display: "block", width: "20rem",p: 2, my: 2}}
                     onClick = {() => handleSelect(options[3])}>
             {options[3]}
           </Button>
-      </div>
+      </div  >
       <div>
-        <Button variant="outlined" color="success"  sx={{ width: "5rem", p: 1, mx: 2 }}>
+        <Button variant="outlined" color="success"  sx={{ width: "5rem", p: 1, mx: 2 }} disabled = {!answer ? true: false} onClick = {handleSubmit}>
           Submit
         </Button>
-        <Button variant="outlined"  sx={{ width: "5rem", p: 1, mx: 2 }}  onClick={nextQ}>
+        <Button variant="outlined"  sx={{ width: "5rem", p: 1, mx: 2 }}  onClick={handleNext} disabled = {!submit ? true: false}  >
           Next
         </Button>
       </div>
